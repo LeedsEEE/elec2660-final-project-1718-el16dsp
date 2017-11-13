63e085f14191dae1b12b3c5c19d4c4cb0b1291d5
Daniel Piper [el16dsp] <el16dsp@eentpc99.leeds.ac.uk>
2017-11-13 10:07:37 +0000

    Added methods and init to PlayerClass

--- FinalProjectGame/PlayerClass.m
+++ FinalProjectGame/PlayerClass.m
@@ -10,4 +10,35 @@
 
 @implementation PlayerClass
 
+- (instancetype)init
+{
+    self = [super init];
+    if (self) {
+        // Put placeholder's here
+        self.Name = @"PlaceholderClass";
+        self.ImageBasis = @"class_placeholder";
+        self.MaxHealth = 0;
+        self.CurrentHealth = 0;
+        self.Armour = 0;
+        self.Button1 = [[WeaponClass alloc] init];
+        self.Button2 = [[WeaponClass alloc] init];
+    }
+    return self;
+}
+
+-(NSInteger) ReturnHealth {
+    if (self.CurrentHealth < 0) {
+        self.CurrentHealth = 0;
+    }
+    return self.CurrentHealth;
+}
+
+-(float) CalculateDamage:(NSInteger)RawDamageIn {
+    float damage_taken;
+    float numerator = 100*RawDamageIn;
+    float denominator = 100+self.Armour;
+    damage_taken = numerator/denominator;
+    return damage_taken;
+}
+
 @end
