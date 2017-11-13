39adf22da8406de75eec066487a23fba7e839986
Daniel Piper [el16dsp] <el16dsp@eentpc99.leeds.ac.uk>
2017-11-13 12:23:14 +0000

    Bulked implementations of DataStore and started making first class (Cutter)

--- FinalProjectGame/DataStore.m
+++ FinalProjectGame/DataStore.m
@@ -7,6 +7,7 @@
 //
 
 #import "DataStore.h"
+#import "PlayerClass.h"
 
 @implementation DataStore
 
@@ -14,18 +15,44 @@
 {
     self = [super init];
     if (self) {
-        self.PlayerClassArray = [[NSMutableArray alloc] init];
+        self.PlayerClassArray = [NSMutableArray array];
+        
+        // Make placeholder classes here
+        PlayerClass *class_0 = [[PlayerClass alloc] init];
+        PlayerClass *class_1 = [[PlayerClass alloc] init];
+        
+        [self LoadData];
+        
+        // Fill in classes here
+        class_0.Name = @"Cutter";
+        class_0.ImageBasis = @"class_0";
+        class_0.MaxHealth = 40;
+        class_0.Armour = 0;
+        
+        class_0.Button1.Type = @"W";
+        class_0.Button1.Name = @"Pistol";
+        class_0.Button1.ImageName = @"pistol.png"; // TODO Add picture to supoprting files
+        class_0.Button1.Description = @"A basic weapon for a basic pirate";
     }
     return self;
 }
 
 -(NSInteger) LoadData {
     // Find out how to find class level data and import that
+    // taken from http://www.ios-blog.co.uk/tutorials/read-text-files-in-ios-objective/
+    NSString *path = [[NSBundle mainBundle] pathForResource:@"class_levels_data"
+                                                     ofType:@"txt"];
+    NSLog(@"%@", path);
+    NSString *content = [NSString stringWithContentsOfFile:path
+                                                  encoding:NSUTF8StringEncoding
+                                                     error:NULL];
+    NSLog(@"%@", content);
     return 0;
 }
 
 -(NSInteger) SaveData {
-    // Save class levels into txt file 
+    // Save class levels into json file
+    return 0;
 }
 
 @end
