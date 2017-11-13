beb59b40c1da99538b15840e3a2bb6da8bc67b99
Daniel Piper [el16dsp] <el16dsp@eentpc99.leeds.ac.uk>
2017-11-13 10:48:28 +0000

    Created ObstacleClass

---
+++ FinalProjectGame/ObstacleClass.m
@@ -0,0 +1,13 @@
+//
+//  ObstacleClass.m
+//  FinalProjectGame
+//
+//  Created by Daniel Piper [el16dsp] on 13/11/2017.
+//  Copyright © 2017 University of Leeds. All rights reserved.
+//
+
+#import "ObstacleClass.h"
+
+@implementation ObstacleClass
+
+@end
