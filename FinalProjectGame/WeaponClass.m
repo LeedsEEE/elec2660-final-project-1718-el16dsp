71473d35b6d5393040212e662f507706a6cb8dc0
Daniel Piper [el16dsp] <el16dsp@eentpc99.leeds.ac.uk>
2017-11-13 09:44:00 +0000

    Added init and the following methods to WeaponClass: DamageDealtOnClick, UpdateStats, AutoIncrement and ManualIncrement

--- FinalProjectGame/WeaponClass.m
+++ FinalProjectGame/WeaponClass.m
@@ -10,4 +10,56 @@
 
 @implementation WeaponClass
 
+- (instancetype)init
+{
+    self = [super init];
+    if (self) {
+        
+    }
+    return self;
+}
+
+-(NSInteger) DamageDealtOnClick {
+    // Called on any button press:
+        // Called on obstacle button press if weapon. Ability won't do anything unless charged
+        // Called on both ability button presses
+        // Any weapon buttons need a catchout to stop this from activating if manual increment occurs
+    if ([self.Type isEqualToString:@"W"]) {
+        if (self.ClickAmount>0) {
+            self.ClickAmount -= 1;
+            return (self.DamagePerClick, self.StunDuration);
+        }
+    } else if ([self.Type isEqualToString:@"A"]) {
+        if (self.ClickAmount == self.ClicksPerClip) {
+            self.ClickAmount = 0;
+            return (self.DamagePerClick, self.StunDuration);
+        }
+    }
+    return (0, 0);
+}
+
+-(void) UpdateStats {
+    // Called only once upgrade purchased
+    NSInteger level;
+    
+    level = self.Level;
+    
+    // Needs to be updated and overridden for each player class ability
+    self.DamagePerClick = 1;
+    self.ClicksPerClip = 1;
+    self.StunDuration = 1;
+    self.AutoClickLoadRate = 1;
+    self.ClickAmount = self.ClicksPerClip;
+}
+
+-(void) AutoIncrement {
+    // Called on any button press in the game
+    self.ClickAmount += self.AutoClickLoadRate;
+}
+
+-(void) ManualIncrement {
+    // Called on the corresponding weapon or ability button press
+    self.ClickAmount += 1;
+}
+
 @end
