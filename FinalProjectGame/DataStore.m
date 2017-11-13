60661fe34abf93c04ae022a7329ea9bd766cf4e7
Daniel Piper [el16dsp] <el16dsp@eentpc99.leeds.ac.uk>
2017-11-13 13:21:49 +0000

    Added a comment to DataStore. Need to create sub-classes for each weapon for each class

--- FinalProjectGame/DataStore.m
+++ FinalProjectGame/DataStore.m
@@ -24,6 +24,7 @@
         [self LoadData];
         
         // Fill in classes here
+        // As it turns out, each class will need it's own weapon subclasses to override them.
         class_0.Name = @"Cutter";
         class_0.ImageBasis = @"class_0";
         class_0.MaxHealth = 40;
