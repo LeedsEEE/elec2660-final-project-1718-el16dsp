1f924365672a100467f7fd300dd6a7adc2c2e32d
Daniel Piper [el16dsp] <el16dsp@eentpc99.local>
2017-11-13 09:13:54 +0000

    Created WeaponClass

---
+++ FinalProjectGame/WeaponClass.m
@@ -0,0 +1,13 @@
+//
+//  WeaponClass.m
+//  FinalProjectGame
+//
+//  Created by Daniel Piper [el16dsp] on 13/11/2017.
+//  Copyright © 2017 University of Leeds. All rights reserved.
+//
+
+#import "WeaponClass.h"
+
+@implementation WeaponClass
+
+@end
