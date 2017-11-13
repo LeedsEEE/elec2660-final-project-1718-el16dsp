47dbe8347941d46fcf579a622e69057e8b4e0bdb
Daniel Piper [el16dsp] <el16dsp@eentpc99.leeds.ac.uk>
2017-11-13 11:53:26 +0000

    Added ObstacleClass variable and method definitions and implementations Changed coin label text to 'Coins'

--- FinalProjectGame/ObstacleClass.h
+++ FinalProjectGame/ObstacleClass.h
@@ -7,7 +7,23 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "WeaponClass.h"
 
 @interface ObstacleClass : NSObject
+
+@property (strong, nonatomic) NSString *Name;
+@property (strong, nonatomic) NSString *ImageBasis;
+
+@property (nonatomic) NSInteger Level;
+@property (nonatomic) NSInteger MaxHealth;
+@property (nonatomic) NSInteger CurrentHealth;
+@property (nonatomic) NSInteger Armour;
+@property (nonatomic) NSInteger Reward;
+@property (strong, nonatomic) WeaponClass *Ability;
+
+-(void) GenerateEnemy:(NSInteger) DesiredLevel;
+-(void) GenerateDoor:(NSInteger) DesiredLevel;
+-(void) GenerateChest:(NSInteger) DesiredLevel;
+-(NSInteger) ReturnHealth;
 
 @end
--- FinalProjectGame/ObstacleClass.m
+++ FinalProjectGame/ObstacleClass.m
@@ -10,4 +10,73 @@
 
 @implementation ObstacleClass
 
+- (instancetype)init:(NSInteger) InputLevel
+{
+    self = [super init];
+    if (self) {
+        NSInteger RandomResult = arc4random() % 3;
+        
+        self.Ability = [[WeaponClass alloc] init];
+        
+        if (RandomResult == 0) { // Door
+            [self GenerateDoor:InputLevel];
+        } else if (RandomResult == 1) { // Chest
+            [self GenerateChest:InputLevel];
+        } else { // Enemy
+            [self GenerateEnemy:InputLevel];
+        }
+    }
+    return self;
+}
+
+-(NSInteger) ReturnHealth {
+    if (self.CurrentHealth < 0) {
+        self.CurrentHealth = 0;
+    }
+    return self.CurrentHealth;
+}
+
+-(void) GenerateChest:(NSInteger)DesiredLevel {
+    self.Name = @"Chest";
+    self.ImageBasis = @"chest";
+    self.Level = DesiredLevel;
+    self.MaxHealth = (6+(arc4random() % 5))*self.Level; // (6 to 10) * level
+    self.CurrentHealth = self.MaxHealth;
+    self.Armour = 0;
+    self.Reward = (10+(arc4random() % 5))*self.Level; // (11 to 20) * level
+    
+    self.Ability.DamagePerClick = 0;
+    self.Ability.AutoClickLoadRate = 0;
+}
+
+-(void) GenerateDoor:(NSInteger)DesiredLevel {
+    self.Name = @"Door";
+    self.ImageBasis = @"door";
+    self.Level = DesiredLevel;
+    self.MaxHealth = (6+(arc4random() % 5))*self.Level; // (6 to 10) * level
+    self.CurrentHealth = self.MaxHealth;
+    self.Armour = 0;
+    self.Reward = ((arc4random() % 5) + 1)*self.Level; // (1 to 5) * level
+    
+    self.Ability.DamagePerClick = 0;
+    self.Ability.AutoClickLoadRate = 0;
+}
+
+-(void) GenerateEnemy:(NSInteger)DesiredLevel {
+    self.Name = @"Enemy";
+    self.ImageBasis = @"enemy";
+    self.Level = DesiredLevel;
+    self.MaxHealth = (6+(arc4random() % 5))*self.Level; // (6 to 10) * level
+    self.CurrentHealth = self.MaxHealth;
+    self.Armour = arc4random() % self.Level; // (0 to level)
+    self.Reward = ((arc4random() % 5) + 1)*self.Level; // (1 to 5) * level
+    
+    self.Ability.Level = DesiredLevel;
+    self.Ability.ClicksPerClip = roundf((float)(self.Level / ((arc4random() % self.Level) + 1)));
+    self.Ability.DamagePerClick = roundf((float)(self.Level / ((arc4random() % self.Level) + 1)));
+    self.Ability.ClickAmount = arc4random() % self.Ability.ClicksPerClip; // Random amount of readiness
+    self.Ability.StunDuration = 0;
+    self.Ability.AutoClickLoadRate = 1;
+}
+
 @end
