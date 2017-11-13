2d1b706d7c1a50025308deabea788e803e10c621
Daniel Piper [el16dsp] <el16dsp@eentpc99.leeds.ac.uk>
2017-11-13 09:49:54 +0000

    Added PlayerClass header and main file

---
+++ FinalProjectGame/PlayerClass.h
@@ -0,0 +1,13 @@
+//
+//  PlayerClass.h
+//  FinalProjectGame
+//
+//  Created by Daniel Piper [el16dsp] on 13/11/2017.
+//  Copyright © 2017 University of Leeds. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PlayerClass : NSObject
+
+@end
---
+++ FinalProjectGame/PlayerClass.m
@@ -0,0 +1,13 @@
+//
+//  PlayerClass.m
+//  FinalProjectGame
+//
+//  Created by Daniel Piper [el16dsp] on 13/11/2017.
+//  Copyright © 2017 University of Leeds. All rights reserved.
+//
+
+#import "PlayerClass.h"
+
+@implementation PlayerClass
+
+@end
