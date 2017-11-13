6b7bff3b2d21ebd475396a3bbbfe137483a0c054
Daniel Piper [el16dsp] <el16dsp@eentpc99.leeds.ac.uk>
2017-11-13 09:28:00 +0000

    Added variables and method definitions to the WeaponClass header file

--- FinalProjectGame/WeaponClass.h
+++ FinalProjectGame/WeaponClass.h
@@ -10,4 +10,28 @@
 
 @interface WeaponClass : NSObject
 
+// Type is used to determine how it operates in gameplay
+@property (strong, nonatomic) NSString *Type; // Will be either @"A" or @"W"
+// 'W' or weapon types will consume one 'click' per use and will do one set of damage. They need to be topped up by pressing on it's button
+// 'A' or ability types will need to be charged up by pressing the button a few times then do an effect on the obstacle
+
+// Non-necessary stuff in the sense that these don't affect gameplay but will be used in the inspection view
+@property (strong, nonatomic) NSString *Name;
+@property (strong, nonatomic) NSString *ImageName;
+@property (strong, nonatomic) NSString *Description;
+
+// These values will directly affect gameplay and will be tailored to each player class
+@property (nonatomic) NSInteger Level; // The following values scale up with this. This can be increased by paying a number of coins
+@property (nonatomic) NSInteger LevelsPerUpgrade; // Is used for determining the cost of the upgrade
+@property (nonatomic) NSInteger DamagePerClick; // How much damage is done per use
+@property (nonatomic) NSInteger ClicksPerClip; // How many clicks can this store
+@property (nonatomic) NSInteger ClickAmount; // A value that, in gameplay, will vary between 0 and ClicksPerClip
+@property (nonatomic) NSInteger StunDuration; // Affects the obstacle only and stops it from attacking
+@property (nonatomic) NSInteger AutoClickLoadRate; // On any click, this will add it's value to ClickAmount
+
+-(NSInteger) DamageDealtOnClick;
+-(void) UpdateStats;
+-(void) AutoIncrement;
+-(void) ManualIncrement;
+
 @end
